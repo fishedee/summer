1636ae5edd0e0cd649e2e42cdaf63b66a5d6f8f6
fishedee <306766045@qq.com>
2017-11-12 22:36:24 +0800

    runtime测试

--- main.go
+++ main.go
@@ -3,18 +3,32 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"runtime"
 )
+
+type MM struct {
+}
+
+func (this *MM) Run() {
+
+}
 
 type UserAo interface {
 	Get(id int) int
 }
 
-func do(v interface{}) {
-	a := reflect.ValueOf(v)
-	fmt.Println(a)
-	fmt.Println(a.Type())
+func do(a int) int {
+	return a + 1
 }
 
 func main() {
-	do((UserAo).Get)
+	targetType := reflect.TypeOf(do)
+	mm := reflect.MakeFunc(targetType, func(args []reflect.Value) (results []reflect.Value) {
+		return []reflect.Value{reflect.ValueOf(3)}
+	})
+	dd := (mm.Interface()).((func(int) int))
+	fmt.Println(dd(34))
+	fmt.Println(targetType.Name())
+	//mc := &MM{}
+	fmt.Println(runtime.FuncForPC(mm.Pointer()).Name())
 }
